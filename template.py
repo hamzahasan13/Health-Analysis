f6912ae00fdf1d457397e043b4f6b38aefc50105
Hamza <hamzahasan0713@gmail.com>
2024-08-12 16:27:37 -0400

    data_ingestion

--- template.py
+++ template.py
@@ -5,16 +5,18 @@ import logging
 #logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+project_name = 'cnnClassifier'
+
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src_code/__init__.py",
-    f"src_code/components/__init__.py",
-    f"src_code/utils/__init__.py",
-    f"src_code/config/__init__.py",
-    f"src_code/config/configuration.py",
-    f"src_code/pipeline/__init__.py",
-    f"src_code/entity/__init__.py",
-    f"src_code/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
@@ -23,7 +25,9 @@ list_of_files = [
     "research/trials.ipynb",
     "templates/Xray.html"
 
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
