7da0dc06b81642915ab5bf88c2923fac3d5a51d4
Hamza <hamzahasan0713@gmail.com>
2024-08-13 11:48:29 -0400

    base_model

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+import tensorflow as tf
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+import ssl
+import urllib.request
+
+ssl._create_default_https_context = ssl._create_unverified_context
 #from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
-
 
 
 
@@ -12,6 +16,20 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   print('1')
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   print('2')
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
