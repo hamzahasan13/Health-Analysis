8c0e3d7e94f473da727c06b8ca02f7bed231c528
Hamza <hamzahasan0713@gmail.com>
2024-08-13 12:41:48 -0400

    model_training

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 import tensorflow as tf
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 import ssl
-import urllib.request
-
 ssl._create_default_https_context = ssl._create_unverified_context
-#from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
 #from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -30,6 +29,18 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    print('2')
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+   
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
